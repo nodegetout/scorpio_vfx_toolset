9fcea8b859fac9c21cdf393fdd7b7e96e04f697c
Nodegetout <844134523@qq.com>
2025-12-21 20:07:52 +0800

    update unity3d

---
+++ Packages/com.scorpio.vfxtoolset/Editor/ShaderGUI/MaterialPropertyDrawer/ModuleKeywordToggleDrawer.cs
@@ -0,0 +1,14 @@
+namespace com.scorpio.vfxtoolset.Editor
+{
+    public class ModuleKeywordToggleDrawer : ModuleToggleUIDrawer
+    {
+        public ModuleKeywordToggleDrawer()
+        {
+        }
+        
+        public ModuleKeywordToggleDrawer(string keyword) : base(keyword)
+        {
+        }
+        
+    }
+} 
